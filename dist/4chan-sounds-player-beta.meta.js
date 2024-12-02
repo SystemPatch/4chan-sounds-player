6094e1f451a00f54acd99a8b6afd18967ffa0f28
SystemPatch <SystemPatch@users.noreply.github.com>
2024-12-02 06:17:47 +0000

    Automated compile

--- dist/4chan-sounds-player-beta.meta.js
+++ dist/4chan-sounds-player-beta.meta.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         4chan sounds player
-// @version      3.5.6-5870ab7
+// @version      3.5.6-23607d4
 // @namespace    rccom
 // @description  A player designed for 4chan sounds threads.
 // @author       RCC
